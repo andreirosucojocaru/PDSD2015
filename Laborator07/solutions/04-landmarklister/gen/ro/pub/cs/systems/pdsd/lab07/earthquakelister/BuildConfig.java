8b134e9838d6e1a466c39a341746bbe3ce25c64d
Andrei Roșu-Cojocaru <andrei.rosucojocaru@gmail.com>
2015-05-20 12:29:32 +0300

    added source code for labs 06-10

---
+++ Laborator07/solutions/04-landmarklister/gen/ro/pub/cs/systems/pdsd/lab07/earthquakelister/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package ro.pub.cs.systems.pdsd.lab07.earthquakelister;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
